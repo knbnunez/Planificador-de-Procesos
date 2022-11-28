e80d298c5817a39acf59b480a7cf83c4158bfdb1
knbnunez <knbnunez@gmail.com>
2022-11-28 17:25:40 -0300

    a

--- Frontend/src/app/results/results.component.ts
+++ Frontend/src/app/results/results.component.ts
@@ -12,14 +12,10 @@ export class ResultsComponent implements OnInit {
   eventos: any = [];
   resultados: any = [];
 
-  constructor(private _servicio:ProcesosService) { }
+  constructor(private _servicio: ProcesosService) { }
 
   ngOnInit() {
-    // llamada al servicio get para traerme los datos
-    // getEventos().suscrbe
-    // getResultados()
     
-    // Acá debería ir el Post, no los get
     this._servicio.getEventos().subscribe(data => {
       console.log(data);
       this.eventos = data;
